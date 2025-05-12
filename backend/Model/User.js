8708f4d081a8a675548349da2bee59d5c1f6ca9a
GILBERT\Gilbe <Huynhbatan0987@gmail.com>
2025-05-12 22:05:55 +0700

    change phone from string to Numbet

--- backend/Model/User.js
+++ backend/Model/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const User = new mongoose.Schema({
     username: { type: String, required: true, },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true, unique: true },
+    phone: { type: Number, required: true, unique: true },
     password: { type: String, required: true },
     thumnail: { type: String, } //If user donn't use thumnail, set default
 });
